e4c5b848f0300fae88fe2af04573293bcd4e8bef
clawrobot <langfordevana@gmail.com>
2025-02-09 21:21:12 -0600

    CAT

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ document.getElementById('changeTextBtn').addEventListener('click', function() {
     document.getElementById('message').textContent = 'You clicked the button!';
 });
 function toggleImage(){
-    let img = document.getElementById(kitty);
+    let img = document.getElementById("kitty");
     img.style.display = (img.style.display === "none" || img.style.display === "") ? "block" : "none";
 }
